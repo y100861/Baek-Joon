a64af551327bba4cf2a17d4f5742d265a0b90e4b
y100861 <107607076+y100861@users.noreply.github.com>
2024-03-31 18:06:28 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 512 ms, Memory: 38008 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(i, j):
+    q = deque()
+    q.append((i, j))
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if not (0<=nx<n and 0<=ny<m):
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 2:
+            bfs(i, j)
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            print(0, end=" ")
+        else:
+            print(graph[i][j] - 2, end=" ")
+    print()
