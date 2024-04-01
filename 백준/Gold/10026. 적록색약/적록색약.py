9937468cf8a4581cc873efab674c3531cd87c1b6
y100861 <107607076+y100861@users.noreply.github.com>
2024-04-01 17:07:22 +0900

    [Gold V] Title: 적록색약, Time: 176 ms, Memory: 114820 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,5 +1,4 @@
 import sys
-import copy
 from collections import deque
 
 input = sys.stdin.readline
@@ -10,47 +9,45 @@ graph = []
 visited = []
 for i in range(n):
     graph.append(list(input().rstrip()))
+    visited.append([0] * n)
 
-graph_2 = copy.deepcopy(graph)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == "R":
-            graph_2[i][j] = "G"
-
-dx = [-1, 1, 0, 0, 0]
-dy = [0, 0, -1, 1, 0]
-
-def bfs(i, j, a, graph):
+def bfs(i, j):
     q = deque([(i, j)])
+    visited[i][j] = 1
+    color = graph[i][j]
+
     while q:
         x, y = q.popleft()
-        for i in range(5):
+        for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if not (0<=nx<n and 0<=ny<n):
                 continue
-            if graph[nx][ny] == a:
-                graph[nx][ny] = 0
+            if graph[nx][ny] == color and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
                 q.append((nx, ny))
 
-r = 0
-g_1 = 0
-g_2 = 0
-b = 0 
+cnt_1 = 0
+for i in range(n):
+    for j in range(n):
+        if visited[i][j] == 0:
+            cnt_1 += 1
+            bfs(i, j)
+
 for i in range(n):
     for j in range(n):
         if graph[i][j] == "R":
-            r += 1
-            bfs(i, j, "R", graph)
-        if graph[i][j] == "G":
-            g_1 += 1
-            bfs(i, j, "G", graph)
-        if graph_2[i][j] == "G":
-            g_2 += 1
-            bfs(i, j, "G", graph_2)
-        if graph[i][j] == "B":
-            b += 1
-            bfs(i, j, "B", graph)
+            graph[i][j] = "G"
 
-print(r+g_1+b, g_2+b)
+visited = [[0] * n for _ in range(n)]
+
+cnt_2 = 0
+for i in range(n):
+    for j in range(n):
+        if visited[i][j] == 0:
+            cnt_2 += 1
+            bfs(i, j)
+print(cnt_1, cnt_2)
