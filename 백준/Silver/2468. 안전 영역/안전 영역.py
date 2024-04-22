21be477425d621352b55e6a061d3035468e72329
y100861 <107607076+y100861@users.noreply.github.com>
2024-04-22 15:27:16 +0900

    [Silver I] Title: 안전 영역, Time: 264 ms, Memory: 116196 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,51 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(i, j):
+    q = deque([(i, j)])
+    visited[i][j] = 0
+
+    while q:
+        x, y = q.popleft()
+
+        for v in range(4):
+            nx = x + dx[v]
+            ny = y + dy[v]
+
+            if not (0<=nx<n and 0<=ny<n):
+                continue
+
+            if visited[nx][ny] == 1:
+                visited[nx][ny] = 0
+                q.append((nx, ny))
+
+result = 0
+for k in range(min(map(min, graph)) - 1, max(map(max, graph))):
+    visited = [[1] * n for _ in range(n)]
+
+    for a in range(n):
+        for b in range(n):
+            if graph[a][b] <= k:
+                visited[a][b] = 0
+
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == 1:
+                cnt += 1
+                bfs(i, j)
+    
+    result = max(result, cnt)
+
+print(result)
